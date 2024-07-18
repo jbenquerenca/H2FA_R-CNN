aa10b6bfcc8c2179603c089ef6e7a09096216982
JoaoBenquerenca <jbenquerenca9@gmail.com>
2024-07-18 12:14:11 +0100

    add docker

---
+++ docker/build.sh
@@ -0,0 +1,1 @@
+docker build -t h2fa_r-cnn . -f Dockerfile
---
+++ docker/run.sh
@@ -0,0 +1,10 @@
+docker run \
+      --name h2fa_r-cnn-container \
+      --gpus all \
+      -it \
+      -e "color_prompt=yes" \
+      -t \
+      --rm \
+      -v $HOME/dissertacao/models/H2FA_R-CNN:/local \
+      -v $HOME/dissertacao/data:/data \
+      h2fa_r-cnn
